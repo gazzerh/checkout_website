35fb81249dd6fcd398e76f8816f5e1c7c91dd2f6
Garry Harthill <gazzerh@gmail.com>
2020-04-26 16:31:47 +0100

    add app version to website

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 
@@ -6,5 +7,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def webapp():
-    message = "Hello World"
-    return render_template('main.html', message=message)
+    try:    
+        version_file = open('./app_version.txt', 'r')
+        version = version_file.read()
+    except OSError:
+        version = "No version available"
+
+    message = 'Hello World'
+    return render_template('main.html', message=message, version=version)
